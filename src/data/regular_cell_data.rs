cdadb2bb45691d0ee67fa69ababfdd9139d96446
Briggs <b3apollo13@gmail.com>
2020-02-16 18:38:45 -0600

    Added docs for regular_cell_data.rs

--- src/data/regular_cell_data.rs
+++ src/data/regular_cell_data.rs
@@ -1,18 +1,25 @@
+/// Holds information about the triangulation used for a single equivalence class in the modified
+/// Marching Cubes algorithm.
 pub struct RegularCellData {
+    /// High nibble is vertex count; low nibble is triangle count.
     geometry_counts: u8,
+    /// Groups of 3 indices giving the triangulation.
     vertex_index: [u8; 15],
 }
 
 impl RegularCellData {
+    /// Gets the vertex count from `RegularCellData::geometry_counts`.
     pub fn get_vertex_count(&self) -> u8 {
         self.geometry_counts >> 4
     }
 
+    /// Gets the triangle count from `RegularCellData::geometry_counts`.
     pub fn get_triangle_count(&self) -> u8 {
         self.geometry_counts & 0x0F
     }
 }
 
+/// Maps an 8-bit regular Marching Cubes case index to an equivalence class index.
 pub const REGULAR_CELL_CLASS: [u8; 256] = [
     0x00, 0x01, 0x01, 0x03, 0x01, 0x03, 0x02, 0x04, 0x01, 0x02, 0x03, 0x04, 0x03, 0x04, 0x04, 0x03,
     0x01, 0x03, 0x02, 0x04, 0x02, 0x04, 0x06, 0x0C, 0x02, 0x05, 0x05, 0x0B, 0x05, 0x0A, 0x07, 0x04,
@@ -32,8 +39,10 @@ pub const REGULAR_CELL_CLASS: [u8; 256] = [
     0x03, 0x04, 0x04, 0x03, 0x04, 0x03, 0x0D, 0x01, 0x04, 0x0D, 0x03, 0x01, 0x03, 0x01, 0x01, 0x00,
 ];
 
+/// Holds the triangulation data for all 16 distinct classes to which a case can be mapped by the
+/// `REGULAR_CELL_CLASS` table.
 pub const REGULAR_CELL_DATA: [RegularCellData; 16] = [
-    RegularCellData {geometry_counts: 0x00, vertex_index: [0; 15]},
+    RegularCellData {geometry_counts: 0x00, vertex_index: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
     RegularCellData {geometry_counts: 0x31, vertex_index: [0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
     RegularCellData {geometry_counts: 0x62, vertex_index: [0, 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
     RegularCellData {geometry_counts: 0x42, vertex_index: [0, 1, 2, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
@@ -51,6 +60,11 @@ pub const REGULAR_CELL_DATA: [RegularCellData; 16] = [
     RegularCellData {geometry_counts: 0x95, vertex_index: [0, 4, 5, 0, 3, 4, 0, 1, 3, 1, 2, 3, 6, 7, 8]},
 ];
 
+/// Gives the vertex locations for every one of the 256 possible cases in the modified Marching
+/// Cubes algorithm.  Each 16-bit value also provides information about whether a vertex can be
+/// reused from a neighboring cell.
+/// The low byte contains the indices for the two endpoints of the edge on which the vertex lies.
+/// The high byte contains the vertex reuse data.
 pub const REGULAR_VERTEX_DATA: [[u16; 12]; 256] = [
     [0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000],
     [0x6201, 0x5102, 0x3304, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000],
